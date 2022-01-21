37c994c2d3188b99f4e85ec2eece7b6866ec23a5
Chrispolanco <chrispolanco@sbcglobal.net>
2022-01-20 21:35:31 -0800

    ui for dropdown added

--- src/components/Dropdown.js
+++ src/components/Dropdown.js
@@ -1,7 +1,30 @@
 import React from 'react'; 
 
-const Dropdown = () => {
-    return <h1>Dropdown</h1>
+const Dropdown = ({options}) => {
+    const rederedOptions = options.map((options) =>{
+        return(
+            <div key={options.value} className='item'>
+                {options.label}
+            </div>
+        ); 
+    }); 
+
+    return(
+        <div className='ui form'>
+            <div className='field'>
+                <label className='label'>Select a Color</label>
+                <div className='ui selection dropdown visible active'>
+                    <i className='dropdown icon'></i>
+                    <div className='text'>
+                        Select Color
+                    </div>
+                    <div className='menu visible transition'>
+                        {rederedOptions}
+                    </div>
+                </div>
+            </div>
+        </div>
+    ); 
 }; 
 
 export default Dropdown; 
